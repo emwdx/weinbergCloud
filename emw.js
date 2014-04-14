8c05ecad978bfa5cfbdd18fab35b257a656c49e2
Evan Weinberg <weinbergmath@gmail.com>
2014-04-14 20:36:33 +0800

    addedProfile

--- emw.js
+++ emw.js
@@ -62,7 +62,7 @@ if (Meteor.isClient) {
       
   });
   
-  
+  Session.set('currentDay',$('#selectDate').val());
   
   Template.mainContent.helpers({
   
@@ -261,7 +261,14 @@ Template.showNextRetakes.events({
 
 Template.allUsers.helpers({
    
-userProfiles: Meteor.users.find({})
+userProfiles: Meteor.users.find({},{sort:{"profile.grade":-1,"profile.realName":1}}),
+ isEvan: function(){
+          
+       return (Meteor.user().emails[0].address=='eweinberg@scischina.org');  
+          
+          
+      }
+      
 
     
 
@@ -291,7 +298,41 @@ currentUser = Meteor.users.findOne({_id:this._id});
 currentCredit = Credits.findOne({user:currentUser.emails[0].address});
 Credits.remove(currentCredit._id);
     
+},
+'click .editProfile': function(e){
+e.preventDefault();
+currentUser = Meteor.users.findOne({_id:this._id});
+Session.set("currentlySelectedUser",currentUser.emails[0].address);
+Session.set("currentlySelectedID",currentUser._id);
+if(currentUser.profile){
+$('#profileUserName').val(currentUser.profile.realName);
+$('#profileGrade').val(currentUser.profile.grade);
 }
+else{
+    $('#profileUserName').val('');
+    $('#profileGrade').val('0');
+}
+$('#profileEdit').modal('show');
+    
+}
+
+});
+
+Template.editUserProfile.events({
+'click #updateProfile': function(e){
+e.preventDefault();
+currentUserName = $('#profileUserName').val();
+currentUserGrade = $('#profileGrade').val()
+var profileObject = {"profile.realName":currentUserName,"profile.grade":currentUserGrade};
+var currentlySelectedID = Session.get("currentlySelectedID");
+var currentUser = Meteor.users.findOne({_id:currentlySelectedID});
+
+Meteor.users.update({_id:currentUser._id}, {$set:profileObject});
+$('#profileEdit').modal('hide');
+    
+}
+    
+        
     
 });
 Template.creditBadge.helpers({
@@ -300,7 +341,7 @@ numberOfCredits: function(){
      currentUser = Meteor.users.findOne({_id:this._id})
      allCredits = Credits.find({user:currentUser.emails[0].address,used:0}).fetch();
      numOfCredits = allCredits.length;
-     return numOfCredits
+     return numOfCredits;
     
 }
     
@@ -311,15 +352,33 @@ numberOfCredits: function(){
      currentUser = Meteor.user();
      allCredits = Credits.find({user:currentUser.emails[0].address,used:0}).fetch();
      numOfCredits = allCredits.length;
-     return numOfCredits
+     return numOfCredits;
     
 }
     
 });
+
+Template.editUserProfile.helpers({
     
+selectedUser: function(){
+    return Session.get("currentlySelectedUser");
+},
+profileName: function(){
+var currentlySelectedUser = Session.get("currentlySelectedID");
+var currentUser = Meteor.users.findOne({_id:currentlySelectedUser}); 
+if(currentUser.profile){
+return Meteor.users.findOne({user:currentUser}).profile.realName;
+}
+else{
+return '';}
+    
+}
+ 
+});    
+
 Template.loggingIn.helpers({
     
-isLoggingIn: Meteor.isLoggingIn() 
+isLoggingIn: Meteor.isLoggingIn
     
 });
 
@@ -360,5 +419,15 @@ Meteor.publish('credits', function() {
      }
      else{return null};
 }); 
-
+    
+Meteor.users.allow({
+    
+  update: function(){
+      return true;
+      
+      
+      
+  }
+    
+})
 }
