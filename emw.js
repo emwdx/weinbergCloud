d0c83ca1cf5b91c0241cbfc98913db324291347e
Evan Weinberg <weinbergmath@gmail.com>
2014-05-21 16:36:00 +0800

    v521
    Added weinbergCash functions

--- emw.js
+++ emw.js
@@ -437,10 +437,13 @@ Template.gamesOfChance.events({
     
 'click #bet2x':function(e){
  e.preventDefault();
- var bet = $('#myBet').val();
- if(bet>0){
- var choice = Random.choice([1,0]);
+ fee = 0.05;
  var currentUser = Meteor.user();
+ var currentBalance = currentUser.wbBalance;
+ var bet = parseFloat($('#myBet').val());
+ if(bet>0&&(bet<currentBalance)&&currentBalance>0){
+ var choice = Random.choice([1,0]);
+ 
  var result = 0;
  if(choice==1){
   result = 2*bet;
@@ -449,12 +452,14 @@ Template.gamesOfChance.events({
 else{
  result = 0;
 }
-var newBalance = currentUser.wbBalance - bet +result;
+var newBalance = currentUser.wbBalance - bet +(1-fee)*result;
+ Meteor.call('giveEMWCash',fee*result);
  Meteor.users.update({_id:currentUser._id},{$set:{wbBalance:newBalance}});
  var transactionObject = {
      user: currentUser.profile.realName,
      bet:bet,
-     result:result,
+     payoff:'2x',
+     result:(result-bet),
      time: new Date()
  
  };
@@ -464,10 +469,13 @@ WeinbergCash.insert(transactionObject);
 },
 'click #bet3x':function(e){
  e.preventDefault();
- var bet = $('#myBet').val();
- if(bet>0){
- var choice = Random.choice([1,1,1,0,0,0,0,0,0,0]);
+ fee = 0.02;
  var currentUser = Meteor.user();
+ var currentBalance = currentUser.wbBalance;
+ var bet = parseFloat($('#myBet').val());
+ if(bet>0&&(bet<currentBalance)&&currentBalance>0){
+ var choice = Random.choice([1,1,1,0,0,0,0,0,0,0]);
+ 
  var result = 0;
  if(choice==1){
   result = 3*bet;
@@ -476,12 +484,14 @@ WeinbergCash.insert(transactionObject);
 else{
  result = 0;
 }
-var newBalance = currentUser.wbBalance - bet + result;
+var newBalance = currentUser.wbBalance - bet + (1-fee)*result;
+  Meteor.call('giveEMWCash',fee*result);
  Meteor.users.update({_id:currentUser._id},{$set:{wbBalance:newBalance}});
  var transactionObject = {
      user: currentUser.profile.realName,
      bet:bet,
-     result:result,
+     payoff:'3x',
+     result:(result-bet),
      time: new Date()
  
  };
@@ -490,11 +500,14 @@ WeinbergCash.insert(transactionObject);
  }
 },
 'click #bet4x':function(e){
+ fee = 0;
  e.preventDefault();
- var bet = $('#myBet').val();
- if(bet>0){
- var choice = Random.choice([1,0,0,0,0]);
  var currentUser = Meteor.user();
+ var currentBalance = currentUser.wbBalance;
+ var bet = parseFloat($('#myBet').val());
+ if(bet>0&&(bet<currentBalance)&&currentBalance>0){
+ var choice = Random.choice([1,0,0,0]);
+ 
  var result = 0;
  if(choice==1){
   result = 4*bet;
@@ -503,12 +516,14 @@ WeinbergCash.insert(transactionObject);
 else{
  result = 0;
 }
-var newBalance = currentUser.wbBalance - bet +result;
+var newBalance = currentUser.wbBalance - bet +(1-fee)*result;
+ Meteor.call('giveEMWCash',fee*result);
  Meteor.users.update({_id:currentUser._id},{$set:{wbBalance:newBalance}});
  var transactionObject = {
      user: currentUser.profile.realName,
      bet:bet,
-     result:result,
+     payoff:'4x',
+     result:(result-bet),
      time: new Date()
  
  };
@@ -521,7 +536,7 @@ WeinbergCash.insert(transactionObject);
 });
 Template.allWBCTotals.helpers({
    
-WBProfiles: Meteor.users.find({"profile.grade":"10"},{sort:{wbBalance:-1}}),
+WBProfiles: Meteor.users.find({},{sort:{wbBalance:-1}}),
 numOfWB: function(){
  return parseFloat(this.wbBalance).toFixed(2);    
 }
@@ -586,5 +601,17 @@ Meteor.users.allow({
   }
     
 })
+
+Meteor.methods({
+ giveEMWCash: function(amount){
+  if(amount>0){   
+  var emwAccount = Meteor.users.findOne({'emails.0.address':'eweinberg@scischina.org'});
+  Meteor.users.update({_id:emwAccount._id},{$inc:{wbBalance:amount}});
+  }
+ }
+    
+    
+    
+});
 
 }
